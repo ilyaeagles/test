3b7b4817bff3f4c984be1e8f3dd6186d229b9060
ilia <iliaeaglese@gmail.com>
2022-06-28 14:37:44 +0300

    (28.06) 2 lesson

---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as chrome_options
+
+
+@pytest.fixture
+def get_chrome_options():
+    options = chrome_options()
+    options.add_argument('chrome')  # use headless if you do not need UI
+    options.add_argument('--start-maximized')
+    options.add_argument('--window-size=1200,800')
+    return options
+
+
+@pytest.fixture
+def get_webdriver(get_chrome_options):
+    options = get_chrome_options
+    driver = webdriver.Chrome(options=options)
+    return driver
+
+
+@pytest.fixture(scope='function')
+def setup(request, get_webdriver):
+    driver = get_webdriver
+    url = 'https://www.macys.com/'
+    if request.cls is not None:
+        request.cls.driver = driver
+    driver.get(url)
+    yield driver
+    driver.quit()
+
